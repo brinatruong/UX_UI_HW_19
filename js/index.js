36afec5938dd5488510fedf379b5c5229554f80b
brinatruong <brina.truong@gmail.com>
2023-05-03 18:31:37 -0400

    subtle fix

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $(".image1").on("mouseenter", function(){ 
-        $(".meat1").show();
+        $(".meat1").show(500);
     }).on("mouseleave", function(){
-        $(".meat1").hide();
+        $(".meat1").hide(500);
     });
     $(".image2").on("mouseenter", function(){ 
-        $(".meat2").show();
+        $(".meat2").show(500);
     }).on("mouseleave", function(){
-        $(".meat2").hide();
+        $(".meat2").hide(500);
     });
 })
